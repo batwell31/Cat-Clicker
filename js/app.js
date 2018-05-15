7142b5e3e8ce4ad6029dcc0d8fc7a045dce76996
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-05-15 19:45:13 -0400

    add click functionality through javascript for philip

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var clickTracker = document.querySelector('#clickTracker');
 var danicaImg = document.querySelector('#danicaImg');
+var philipImg = document.querySelector('#philipImg');
 var clicks = 0;
 
 danicaImg.addEventListener('click', function(e) {
+    updateClicks();
+});
+
+philipImg.addEventListener('click', function (e) {
+    updateClicks();
+});
+
+function updateClicks() {
     clicks++;
     clickTracker.innerHTML = clicks;
-});
+}
