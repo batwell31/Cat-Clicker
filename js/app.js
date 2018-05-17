bf113ac075938518311421d40f8e3167c685375d
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-05-16 20:33:59 -0400

    target img/name/click divs and clear out html prior to manipulating the DOM

--- js/app.js
+++ js/app.js
@@ -1,26 +1,9 @@
-var danicasClickTracker = document.querySelector('#danicasClickTracker');
-var philipsClickTracker = document.querySelector('#philipsClickTracker');
-var danicaImg = document.querySelector('#danicaImg');
-var philipImg = document.querySelector('#philipImg');
-var danicasClicks = 0;
-var philipsClicks = 0;
 
-danicaImg.addEventListener('click', function (e) {
-    updateDanicasClicks();
-});
 
-philipImg.addEventListener('click', function (e) {
-    updatePhilipsClicks();
-});
+var imgDiv = document.querySelector('#imgDiv'),
+    nameDiv = document.querySelector('#nameDiv'),
+    clickDiv = document.querySelector('#clickDiv');
 
-function updateDanicasClicks() {
-    danicasClicks++;
-    danicasClickTracker.innerHTML = danicasClicks;
-}
 
-function updatePhilipsClicks() {
-    philipsClicks++;
-    philipsClickTracker.innerHTML = philipsClicks;
-}
 
 var catNames=['Briar-rose', 'Danica', 'Franklin', 'Harmony', 'Philip'];
